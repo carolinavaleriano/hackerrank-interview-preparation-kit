5140d74d6b54cd864f2cc6f83f3a925f8b20aead
Carolina Valeriano <102779256+carolinavaleriano@users.noreply.github.com>
2024-06-13 21:10:34 -0300

    feat: arrays resolved

---
+++ Arrays/2dArrayDs.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+    // Write your code here
+    let maxSum = Number.MIN_SAFE_INTEGER;
+
+    for (let i = 1; i < 5; i++) {
+        for (let j = 1; j < 5; j++) {
+            const sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
+            maxSum = Math.max(maxSum, sum);
+        }
+    }
+
+    return maxSum;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = hourglassSum(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
